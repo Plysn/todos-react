c6452a327ee7f9b3ab6ec2e14f14fe823895940b
Plysn <phuonglynguyen61@gmail.com>
2023-04-19 09:35:44 +0700

    login form ui

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "../../css/styles.css";
 
@@ -7,27 +7,43 @@ function Login() {
 
   const handleClick = () => {
     // hanlde condition here
-
-    navigate("/todos");
-
+    // navigate("/todos");
     // errors
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { username, password } = e.target.elements;
+
+    console.log(username.value);
+    console.log(password.value);
+
+    // calling api
+  };
+
+  console.log("rerender");
+
   return (
     <div>
-      <form className="login-form">
+      <form className="login-form" onSubmit={handleSubmit}>
         <span>LOGIN</span>
         <input
           type="text"
           placeholder="Username"
           className="input-login username"
+          name="username"
         />
         <input
           type="password"
           placeholder="Password"
           className="input-login password"
+          name="password"
         />
-        <button className="input-login button" onClick={handleClick}>
+        <button
+          className="input-login button"
+          onClick={handleClick}
+          type="submit"
+        >
           Login
         </button>
       </form>
